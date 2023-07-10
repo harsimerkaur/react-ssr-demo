cc85eb33f0ee17ea579edd559ee64843c7a498d4
harsimer kaur <harsimer.kaur@round.glass>
2023-07-10 18:28:25 +0530

    new project created for understanding React SSR with custom webpack config

---
+++ client/index.js
@@ -0,0 +1,13 @@
+
+// ./client/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+window.addEventListener('DOMContentLoaded', () => {
+  ReactDOM.hydrate(
+    <App />,
+    document.getElementById('ssr-app')
+  );
+}); 
